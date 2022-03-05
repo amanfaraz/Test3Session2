eddaa1856e97f256f85cc1d96f6e3b59d15b4a00
amanfaraz2002 <94587617+amanfaraz@users.noreply.github.com>
2022-03-05 06:45:45 +0000

    not yet finished the program

--- p1final.c
+++ p1final.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+void input(int *num1,int *num2, int *den1,int *den2)
+{
+  printf("Enter the Numerator and Denominator respectively: /t");
+  scanf("%d%d",num1,den1);
+  printf(""Enter the second Numerator and Denominator respectively: /t");
+    scanf("%d%d",num2,den2);
+}
+void add(int num1,int num2,int den1, int den2, int *num3, int *den3);
+if (den1==den2)
+{
+  *num3=num1+num2;
+  *den3=den1;
+}
+else
+{
+  *num3=(num1*den2+num2*den1);
+  *den3=(den1*den2);
+}
+if (*num3>*den3)
+{
+  for(int i=2;i<=(*num3/2);i++)
+    {}
+}
